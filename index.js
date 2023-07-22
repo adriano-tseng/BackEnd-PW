ba44cda1e0bf73d246ee1459a1149dc07c57fc70
adriano-tseng <adriano>
2023-07-21 21:20:01 -0500

    rutas al index

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import cors from 'cors';
 import docentesRoutes from './src/routes/docentes.js';
 import estudiantesRoutes from './src/routes/estudiantes.js';
 import citasRoutes from './src/routes/citas.js'
+import cursoRoutes from './src/routes/curso.js'
+import calificacionRoutes from './src/routes/calificacion.js'
+import horarioRoutes from './src/routes/horario.js'
+import universidadRoutes from './src/routes/universidad.js'
 //
 const PORT = process.env.PORT || 3000
 
@@ -27,8 +31,12 @@ app.get('/', (req, res) =>{
 //
 //rutas
 app.use("/docentes", docentesRoutes);
-app.use("/estudiantes", estudiantesRoutes)
-app.use("/citas", citasRoutes)
+app.use("/estudiantes", estudiantesRoutes);
+app.use("/citas", citasRoutes);
+app.use("/calificacion", calificacionRoutes);
+app.use("/curso", cursoRoutes);
+app.use("/horario", horarioRoutes);
+app.use("/universidad", universidadRoutes);
 
 app.listen(PORT, () => {
     console.log('Servidor iniciado.');
